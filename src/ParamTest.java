b85c221ddac6b430bb31bb173df0fb9b11a091f5
Sheharyar Kalim (U2289807) <U2289807@unimail.hud.ac.uk>
2023-01-16 15:56:30 +0000

    exercise one is done

--- src/ParamTest.java
+++ src/ParamTest.java
@@ -1,2 +1,63 @@
-package PACKAGE_NAME;public class ParamTest {
+class ParamTest {
+
+    public static void main(String args[]) {
+
+        int val;
+
+        String s1, s2;
+
+        val = 10;
+
+        s1 = "apples";
+
+        s2 = "pears";
+
+
+
+        System.out.println("val is " + val);
+
+        System.out.println("s1 is "  + s1);
+
+        System.out.println("s2 is " + s2);
+
+
+
+        System.out.println("Calling modify ..");
+
+        // All parameters passed by value
+
+        modify(val, s1, s2);
+
+
+
+        System.out.println("returned from modify");
+
+        System.out.println("val is " + val);
+
+        System.out.println("s1 is " + s1);
+
+        System.out.println("s2 is " + s2);
+
+    }
+
+
+
+    public static void modify(int a, String r1, String r2) {
+
+        System.out.println("in modify ...");
+
+        a = 0;
+
+        r1 = null;
+
+        r2 += " taste good";
+
+        System.out.println("a is " + a);
+
+        System.out.println("r1 is " + r1);
+
+        System.out.println("r2 is " + r2);
+
+    }
+
 }
